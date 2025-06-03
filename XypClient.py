f9c389e602f5204bebae55f565b239cbedeb284e
dorjoo <dorjoohover@gmail.com>
2025-06-03 13:13:31 +0800

    a

--- XypClient.py
+++ XypClient.py
@@ -64,39 +64,9 @@ class Service():
     
     def dump(self, operation, params=None):
         try:
-            print({
-                    'request': {
-                        'auth': {
-                            'operator': {
-                                'authType': 0, 
-                                'signature': self.__signature
-                            },
-                            'citizen': {
-                                'authType': 0, 
-                                'signature': self.__signature
-                            }
-                        },
-                        'plateNumber': params['plateNumber']
-                    }
-                })
             if params:
-                response = self.client.service[operation]({
-                    'request': {
-                        'auth': {
-                            'operator': {
-                                'authType': 0, 
-                                'signature': self.__signature
-                            },
-                            'citizen': {
-                                'authType': 0, 
-                                'signature': self.__signature
-                            }
-                        },
-                        'plateNumber': params['plateNumber']
-                    }
-                })
+                response = self.client.service[operation](params)
                 print(response)
-                print(self.client.service[operation]())
             else:
                 print(self.client.service[operation]())
         except Exception as e:
