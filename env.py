63406bc570f6e89929af67ea762df70628b0448f
dorjoo <dorjoohover@gmail.com>
2025-06-05 08:23:49 +0800

    a

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 ACCESS_TOKEN="95b3a82a5ae64c208cfaed0d56b7563c"
-KEY_PATH="mykey.key"
-CERT_PATH="certificate.crt"
+CERT_PATH    = "/root/xyp/certificate.crt"   # Хэрвээ .pem бол тухайн зам
+KEY_PATH     = "/root/xyp/mykey.key"
 REGNUM="ИХ97070415"
 WEBSOCKETURL="ws://127.0.0.1:59001"
