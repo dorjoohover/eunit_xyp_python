bf5de8caf81968d9563858bc6ac6b37ca046ae67
dorjoo <dorjoohover@gmail.com>
2025-07-09 17:24:53 +0800

    a

--- otp.py
+++ otp.py
@@ -6,40 +6,44 @@ from zeep import Client
 from zeep.transports import Transport
 from requests import Session
 import urllib3
+import argparse
+import json
+import os
 import time
 from base64 import b64encode
 from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_v1_5
-from Crypto.PublicKey import RSA
-from env import REGNUM, ACCESS_TOKEN, KEY_PATH
-
-
+from Crypto.PublicKey import RSA 
+from env import ACCESS_TOKEN, KEY_PATH
 class XypSign:
     def __init__(self, KeyPath):
-        self.KeyPath = KeyPath
-
-    def __get_priv_key(self):
+        self.KeyPath = KeyPath 
+    def __GetPrivKey(self):
         with open(self.KeyPath, "rb") as keyfile:
             return RSA.importKey(keyfile.read())
 
+    def __toBeSigned(self, accessToken):
+        return {
+            'accessToken' : accessToken,
+            'timeStamp' : self.__timestamp(),
+        }
+    def __buildParam(self, toBeSigned):        
+        return toBeSigned['accessToken'] + '.' + toBeSigned['timeStamp']
+    def sign(self, accessToken):
+        toBeSigned = self.__toBeSigned(accessToken)
+        digest = SHA256.new()
+        digest.update(self.__buildParam(toBeSigned).encode('utf8'))
+        pkey = self.__GetPrivKey()
+        dd = b64encode(PKCS1_v1_5.new(pkey).sign(digest))
+        return toBeSigned, dd
     def __timestamp(self):
         return str(int(time.time()))
 
-    def sign(self, accessToken):
-        timestamp = self.__timestamp()
-        to_be_signed = f"{accessToken}.{timestamp}"
-        digest = SHA256.new()
-        digest.update(to_be_signed.encode('utf8'))
-        pkey = self.__get_priv_key()
-        signature = b64encode(PKCS1_v1_5.new(pkey).sign(digest))
-        return {'accessToken': accessToken, 'timeStamp': timestamp}, signature
-
 
 class Service:
-    def __init__(self, wsdl, accesstoken, pkey_path):
+    def __init__(self, wsdl, accesstoken, pkey_path=None):
         self.__accessToken = accesstoken
         self.__toBeSigned, self.__signature = XypSign(pkey_path).sign(self.__accessToken)
-
         urllib3.disable_warnings()
         session = Session()
         session.verify = False
@@ -49,29 +53,20 @@ class Service:
         self.client.transport.session.headers.update({
             'accessToken': self.__accessToken,
             'timeStamp': self.__toBeSigned['timeStamp'],
-            'signature': self.__signature.decode('utf-8')
+            'signature': self.__signature
         })
 
-    def call(self, operation, params=None):
+    def dump(self, operation, params=None):
         try:
             if params:
                 response = self.client.service[operation](params)
+                print(response)
             else:
-                response = self.client.service[operation]()
-            print(response)
+                print(self.client.service[operation]())
         except Exception as e:
-            print(f"Error calling {operation}: {e}")
+            print( operation, str(e))
 
+params = { 'plateNumber':'5705УКМ'}
 
-# ❗ REGNUM нь таны улсын бүртгэлийн дугаар (e.g., "УК12345678") байх ёстой.
-params = {
-    'plateNumber': "5705УКМ"
-}
-
-citizen = Service(
-    "https://xyp.gov.mn/transport-1.3.0/ws?WSDL",  # Хэрэв 1.5.0 бол энэ линкийг ашиглана
-    ACCESS_TOKEN,
-    KEY_PATH
-)
-
-citizen.call("WS100401_getVehicleInfo", params)
+citizen = Service('https://xyp.gov.mn/transport-1.3.0/ws?WSDL', ACCESS_TOKEN, KEY_PATH)
+citizen.dump('WS100401_getVehicleInfo', params)
